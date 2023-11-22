13bd89d9fdc95dc272e762ad963e16afac618f10
Ciaowilly3 <muscomassi@gmail.com>
2023-11-22 14:35:23 +0100

    created Model

---
+++ src/main/java/com/cinematic/cinematic/models/Cinema.java
@@ -0,0 +1,26 @@
+package com.cinematic.cinematic.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+@Table(name = "cinema")
+public class Cinema {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cinemaId;
+
+    @Column(name = "cinema_name")
+    private String cinemaName;
+
+    @Column(name = "city")
+    private String city;
+}
