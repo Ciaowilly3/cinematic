8ab16b882db8f36d91aee43db5f099ec17281cf7
Ciaowilly3 <muscomassi@gmail.com>
2023-11-23 18:03:41 +0100

    switching to codeReview branch

--- src/main/java/com/cinematic/cinematic/controllers/FilmController.java
+++ src/main/java/com/cinematic/cinematic/controllers/FilmController.java
@@ -4,19 +4,18 @@ import com.cinematic.cinematic.dtos.FilmDto;
 import com.cinematic.cinematic.mappers.FilmMapper;
 import com.cinematic.cinematic.models.Film;
 import com.cinematic.cinematic.services.impl.FilmServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RequestMapping("films")
+@RequiredArgsConstructor
 @RestController
 public class FilmController {
 
     private final FilmServiceImpl filmService;
-
-    @Autowired
-    public FilmController(FilmServiceImpl filmService){this.filmService = filmService;}
 
     @GetMapping
     public List<FilmDto> retrieveAllFilms(){return FilmMapper.INSTANCE.filmsToFilmsDtos(filmService.retrieveAllFilms());}
@@ -30,8 +29,8 @@ public class FilmController {
     @GetMapping(path = "/{title}")
     public List<FilmDto> retrieveFilmsByTitle(@PathVariable("title") String title){return FilmMapper.INSTANCE.filmsToFilmsDtos(filmService.retrieveFilmsByTitle(title));}
 }
-// TODO: levare api dal path
-// TODO: controllare la dependency injection venga fatta tramite requiredargsconstructor di lombok
+// TODO: levare api dal path FATTO
+// TODO: controllare la dependency injection venga fatta tramite requiredargsconstructor di lombok FATTO
 // TODO: sostituire l'istanza statica di mapstruct con la versione Bean, occhio ad aggiornare l'annotation aggiungendo modelspring
 // TODO: quando la variabile java è uguale a quella tra le graffe non specificare in pathvariable
 // TODO: scrivere path in snakecase;
